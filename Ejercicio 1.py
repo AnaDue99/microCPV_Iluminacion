7f34cad3d0cbe6590f34591a3d208692489fd827
99anaduegor <99anaduegor@gmail.com>
2021-09-23 00:11:11 +0200

    notebook

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -11,11 +11,10 @@ from pvlib import location
 import pandas as pd
 import matplotlib.pyplot as plt
 import math as m
-import numpy as np
 
-"Suponemos una lente paralela al suelo"
+
 caracteristics_module={    
-    'tilt':0,
+    'tilt':30,
     'surface_azimuth':180,
     'focal_distance': 5,
     'tam_module':30
@@ -39,7 +38,20 @@ times = pd.date_range(caracteristics_ETSIDI_summer['date'], freq='10min', period
 clearsky = site.get_clearsky(times)
 solar_position = site.get_solarposition(times)
     
+"Calculamos la irradiancia que le llega"
+"         CARACTERISTICAS DEL      MODULO FISICAS"
 
+  
+total_irrad=irradiance.get_total_irradiance( 
+    surface_tilt=caracteristics_module['tilt'],
+    surface_azimuth=caracteristics_module['surface_azimuth'],
+    dni=clearsky['dni'],
+    ghi=clearsky['ghi'],
+    dhi=clearsky['dhi'],
+    solar_zenith=solar_position['apparent_zenith'],
+    solar_azimuth=solar_position['azimuth'])
+    
+    
 "GET AOI"
 AOI=irradiance.aoi( caracteristics_module['tilt'],
                    caracteristics_module['surface_azimuth'],
@@ -48,15 +60,23 @@ AOI=irradiance.aoi( caracteristics_module['tilt'],
 
 min_AOI=min(AOI)
 max_AOI=max(AOI)
+
 def get_max_aoi(tam_module,focal_dist):
     MAX_AOI=m.atan(tam_module/focal_dist)*180/m.pi
     return MAX_AOI
 MAX_AOI=get_max_aoi(caracteristics_module['tam_module'],caracteristics_module['focal_distance'])    
     
-    
-for x in range(len(AOI)):
+light=[]
+electricity=[]
+
+for x in range(len(AOI)):    
     if AOI[x]>MAX_AOI:
+        light.append(total_irrad['poa_direct'][x])
+        electricity.append(0)
         AOI[x]=MAX_AOI
+    else:
+        electricity.append(total_irrad['poa_direct'][x])
+        light.append(0)
 
 for x in range(len(AOI)):
     if AOI[x]==min_AOI:
@@ -64,6 +84,12 @@ for x in range(len(AOI)):
         for x in range(a,len(AOI)):
           AOI[x]=-AOI[x]
 
+for i in range(len(light)):
+    light[i]=light[i]+total_irrad['poa_diffuse'][i]
+
+    
+    
+    
 AOI.index = AOI.index.strftime("%H:%M")        
 plt.plot(AOI)
 plt.xlabel('Horario  verano')
@@ -73,35 +99,61 @@ plt.show()
 
 
 
-total_irrad=irradiance.get_total_irradiance( 
-        surface_tilt=caracteristics_module['tilt'],
-        surface_azimuth=caracteristics_module['surface_azimuth'],
-        dni=clearsky['dni'],
-        ghi=clearsky['ghi'],
-        dhi=clearsky['dhi'],
-        solar_zenith=solar_position['apparent_zenith'],
-        solar_azimuth=solar_position['azimuth'])
-
-
 DifAzimuths=caracteristics_module['surface_azimuth']-solar_position['azimuth']
-
-
+DifAzimuths=DifAzimuths.tolist()
+[float(i) for i in DifAzimuths]
 "La distancia entre célula y lente será la distancia focal"
+
 def get_r(AOI,focal_dist):
     r=focal_dist*m.tan(AOI*m.pi/180)
     "por el convenio establecido:"
     r=-r
     return r
+
+def get_x(_r,DifAzimuths):
+    x=_r*m.cos(DifAzimuths*m.pi/180)
+    return x
+def get_y(_r,DifAzimuths):
+    y=_r*m.sin(DifAzimuths*m.pi/180)
+    return y
+
 R=[]
+X=[]
+Y=[]
+
 for i in AOI:
     R.append(get_r(i,caracteristics_module['focal_distance']))
 
+[float(i) for i in R]
 
-      
-plt.plot(AOI,R)
-plt.xlabel('AOI (DEG)')
+for i in range(len(R)):
+    X.append(get_x(R[i],DifAzimuths[i]))
+    Y.append(get_y(R[i],DifAzimuths[i]))
+
+    
+
+    
+plt.plot(AOI,R,label='R')
+"plt.plot(AOI,X,label='X')"
+"plt.plot(AOI,Y,label='Y')"
+plt.legend(loc=(1.0, 0))
+plt.xlabel('AOI (º)')
 plt.ylabel('R (mm)')
 plt.title('Movimiento del foco dependiendo del AOI a lo largo del dia')
 plt.show()
+plt.gcf().set_tight_layout(True)
+
+plt.plot(AOI,light, label='Light Irradiance')
+plt.plot(AOI,electricity, label='Electricity Irradiance')
+plt.plot(AOI,total_irrad['poa_global'],label='total irradiance')
+"plt.plot(AOI,total_irrad['poa_diffuse'],label='diffuse')"
+plt.legend(loc=(1.0, 0))
+plt.xlabel('AOI [º]')
+plt.ylabel('Irradiance [W/m2]')
+plt.title('Muesta irradiancias dependiendod el AOI de una lente')
+plt.show()
+plt.gcf().set_tight_layout(True)
+
+        
 
 
