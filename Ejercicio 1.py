d4fa2588f047b9c18ed10656527eb2d4a0f06476
99anaduegor <99anaduegor@gmail.com>
2021-09-30 00:00:33 +0200

    Integramos concepto de SPOT

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 17 13:21:37 2021
+
+Ejercicio 1.0
 "ESTUDIO IRRADIANCIA  SALIENTE DE UNA LENTE BICONVEXA DEPENDIENDO DEL AOI"
 @author: Ana Dueñas
 """
@@ -11,13 +13,15 @@ from pvlib import location
 import pandas as pd
 import matplotlib.pyplot as plt
 import math as m
-
+from scipy.integrate import quad    
 
 caracteristics_module={    
     'tilt':30,
     'surface_azimuth':180,
     'focal_distance': 5,
-    'tam_module':30
+    'tam_module':7,
+    'radius_multijunction':2,
+    'radius_spot':2.1
     }
 
 caracteristics_ETSIDI_summer={
@@ -115,9 +119,7 @@ R=[]
 for i in AOI:
     R.append(get_r(i,caracteristics_module['focal_distance']))
 
-   
-
-    
+     
 plt.plot(AOI,R,label='R')
 
 plt.legend(loc=(1.0, 0))
@@ -139,5 +141,78 @@ plt.show()
 plt.gcf().set_tight_layout(True)
 
         
+"Estudiamos mejor, teniendo en cuenta el concepto de SPOT"
+light_spot=[]
+electricity_spot=[]
 
 
+"Definición interseccion 2 circulos"
+def f(x):
+    return m.sqrt(abs((caracteristics_module['radius_multijunction'])**2-x**2))
+
+def intersection(f,d, rs, rm):
+   
+    B=rm
+    C=abs(d)-rs
+    A=(B+C)/2
+    match_surface=quad(f,A,B)
+  
+    if match_surface[0]<0:
+        match_surface_ = 0 
+          
+    else:
+        match_surface_=4*match_surface[0]
+    return match_surface_
+
+match_surface=[]
+electric_efficiency=[]
+for i in range(len(R)):
+   match_surface.append(intersection(f,R[i],caracteristics_module['radius_spot'],caracteristics_module['radius_multijunction']))
+   
+   if match_surface==0:
+        light_spot.append(total_irrad['poa_direct'][i])
+        electricity_spot.append(0)
+      
+    
+   else:
+        electric_efficiency.append(match_surface[i]/(m.pi*(caracteristics_module['radius_spot'])**2))
+        light_spot.append(total_irrad['poa_direct'][i]*(1-electric_efficiency[i]))
+        electricity_spot.append(total_irrad['poa_direct'][i]*electric_efficiency[i])
+
+        
+for i in range(len(light)):
+    light_spot[i]=light_spot[i]+total_irrad['poa_diffuse'][i]
+
+plt.plot(AOI, match_surface,label='area coincidente')
+plt.plot(AOI, electric_efficiency,label='eficiencia generación electrica')
+plt.show()
+
+plt.plot(AOI,light_spot, label='Light Irradiance')
+plt.plot(AOI,electricity_spot, label='Electricity Irradiance')
+plt.plot(AOI,total_irrad['poa_global'],label='total irradiance')
+"plt.plot(AOI,total_irrad['poa_diffuse'],label='diffuse')"
+plt.legend(loc=(1.0, 0))
+plt.xlabel('AOI [º]')
+plt.ylabel('Irradiance [W/m2]')
+plt.title('Muesta irradiancias dependiendod el AOI de una lente TENIENDO EN CUENTA EL SPOT')
+plt.show()
+plt.gcf().set_tight_layout(True)
+
+
+"""
+if abs(R[i]) > caracteristics_module['radius_spot']+caracteristics_module['radius_multijunction']:
+        light_spot.append(total_irrad['poa_direct'][i])
+        electricity_spot.append(0)
+    
+   elif abs(R[i])< caracteristics_module['radius_multijunction']:
+        electric_efficiency_=caracteristics_module['radius_multijunction']/caracteristics_module['radius_spot']
+        light_spot.append(total_irrad['poa_direct'][i]*(1-electric_efficiency_))
+        electricity_spot.append(total_irrad['poa_direct'][i]*electric_efficiency_)
+    
+   else:
+        match_surface.append(intersection(f,R[i],caracteristics_module['radius_spot'],caracteristics_module['radius_multijunction']))
+        electric_efficiency=abs(match_surface[sur]/(m.pi*(caracteristics_module['radius_spot'])**2))
+        sur=sur+1
+        light_spot.append(total_irrad['poa_direct'][i]*(1-electric_efficiency))
+        electricity_spot.append(total_irrad['poa_direct'][i]*electric_efficiency)
+        """
